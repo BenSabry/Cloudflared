950532f8e04012264c153c949cca8e113a85b3da
BenSabry <BenSabry@outlook.com>
2025-01-15 13:43:51 +0200

    less log and more delay

--- content/etc/periodic/reboot/cloudflared-init.sh
+++ content/etc/periodic/reboot/cloudflared-init.sh
@@ -2,21 +2,19 @@
 echo ""
 echo "$(date) $0 $@"
 
-delay=60
+delay=300
 error="WRN No instances found"
 logs="/root"
 
 service cloudflared restart
 
 while true; do
-    output=$(cloudflared tunnel diag)
+    output=$(cloudflared tunnel diag > /dev/null 2>&1)
     if echo "$output" | grep -q "$error"; then
-        echo "$(date): No instances found. Restarting cloudflared service..."
+        echo "No instances found. Restarting cloudflared service..."
         service cloudflared restart
-    else
-        echo "$(date): cloudflared is running normally."
     fi
 
-    rm "$logs"/*.zip
+    rm "$logs"/*.zip > /dev/null 2>&1
     sleep $delay
 done
